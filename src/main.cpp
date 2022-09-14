2f5d12260571afd52bdb140278f7e3b780550e2c
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2022-09-14 11:51:07 -0400

    Refactoring: Updated API calls to KDDOckWidgets 2.0 development branch.

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@
 */
 
 
-#define SDIO = false
+// #define SDIO = false
 #include <kddockwidgets/Config.h>
 // If DockWidgetQuick isn't found is because KDDockWidgets was compiled without QtQuick support.
-#include <kddockwidgets/DockWidgetQuick.h>
-#include <kddockwidgets/private/DockRegistry_p.h>
-#include <kddockwidgets/FrameworkWidgetFactory.h>
+#include <kddockwidgets/private/DockRegistry.h>
+#include <kddockwidgets/ViewFactory.h>
+#include <kddockwidgets/Platform_qtquick.h>
+#include <kddockwidgets/views/DockWidget_qtquick.h>
+#include "kddockwidgets/views/MainWindow_qtquick.h"
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
@@ -37,40 +39,48 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setOrganizationName("Cuperino");
     QCoreApplication::setOrganizationDomain("cuperino.com");
-    QCoreApplication::setApplicationName("QPrompt Studio");
+    QCoreApplication::setApplicationName("QPrompt");
 
     qmlRegisterType<DocumentHandler>("com.cuperino.qprompt.document", 1, 0, "DocumentHandler");
 
-    auto flags = KDDockWidgets::Config::self().flags();
-    KDDockWidgets::Config::self().setFlags(flags);
+//     auto flags = KDDockWidgets::Config::self().flags();
+//
+// #if defined(DOCKS_DEVELOPER_MODE)
+//     auto internalFlags = KDDockWidgets::Config::self().internalFlags();
+//     // These are debug-only/development flags, which you can ignore.
+//     KDDockWidgets::Config::self().setInternalFlags(internalFlags);
+// #endif
+//
+//     KDDockWidgets::Config::self().setFlags(flags);
 
     QQmlApplicationEngine appEngine;
     // If setQmlEngine isn't found is because KDDockWidgets was compiled without QtQuick support.
-    KDDockWidgets::Config::self().setQmlEngine(&appEngine);
+    KDDockWidgets::Platform_qtquick::instance()->setQmlEngine(&appEngine);
     appEngine.load((QUrl(QStringLiteral("qrc:/qml/main.qml"))));
 
-    auto prompterDockWidget = new KDDockWidgets::DockWidgetQuick("Prompter", KDDockWidgets::DockWidgetBase::Option::Option_NotClosable);
-    prompterDockWidget->setWidget(QStringLiteral("qrc:/PrompterView.qml"));
+    auto prompterDockWidget = new KDDockWidgets::Views::DockWidget_qtquick("Prompter");
+    prompterDockWidget->setGuestItem(QStringLiteral("qrc:/PrompterView.qml"));
     //prompterDockWidget->setWidget(QStringLiteral("qrc:/Prompter.qml"));
     prompterDockWidget->show();
 
-    auto editorDockWidget = new KDDockWidgets::DockWidgetQuick("Editor", KDDockWidgets::DockWidgetBase::Option::Option_NotClosable);
-    editorDockWidget->setWidget(QStringLiteral("qrc:/PrompterView.qml"));
+    auto editorDockWidget = new KDDockWidgets::Views::DockWidget_qtquick("Editor");
+    editorDockWidget->setGuestItem(QStringLiteral("qrc:/PrompterView.qml"));
     editorDockWidget->show();
 
-    auto markersDockWidget = new KDDockWidgets::DockWidgetQuick("Markers");
-    markersDockWidget->setWidget(QStringLiteral("qrc:/MarkersDrawer.qml"));
+    auto markersDockWidget = new KDDockWidgets::Views::DockWidget_qtquick("Markers");
+    markersDockWidget->setGuestItem(QStringLiteral("qrc:/MarkersDrawer.qml"));
     markersDockWidget->show();
 
-    auto toolbarDockWidget = new KDDockWidgets::DockWidgetQuick("Toolbar", KDDockWidgets::DockWidgetBase::Option::Option_NotClosable);
-    toolbarDockWidget->setWidget(QStringLiteral("qrc:/EditorToolbar.qml"));
+    auto toolbarDockWidget = new KDDockWidgets::Views::DockWidget_qtquick("Toolbar");
+    toolbarDockWidget->setGuestItem(QStringLiteral("qrc:/EditorToolbar.qml"));
     toolbarDockWidget->show();
 
-    KDDockWidgets::MainWindowBase *mainWindow = KDDockWidgets::DockRegistry::self()->mainwindows().constFirst();
-    mainWindow->addDockWidgetAsTab(prompterDockWidget);
-    mainWindow->addDockWidget(editorDockWidget, KDDockWidgets::Location_OnLeft, nullptr, QSize(660, 480));
-    mainWindow->addDockWidget(markersDockWidget, KDDockWidgets::Location_OnLeft, nullptr, QSize(320, /*ignored*/0));
-    mainWindow->addDockWidget(toolbarDockWidget, KDDockWidgets::Location_OnBottom, nullptr, QSize(/*ignored*/0, 96));
+    // Access the main area we created in QML with DockingArea {}
+    auto mainArea = KDDockWidgets::DockRegistry::self()->mainDockingAreas().constFirst();
+    mainArea->addDockWidgetAsTab(prompterDockWidget);
+    mainArea->addDockWidget(editorDockWidget, KDDockWidgets::Location_OnLeft, nullptr, QSize(660, 480));
+    mainArea->addDockWidget(markersDockWidget, KDDockWidgets::Location_OnLeft, nullptr, QSize(320, /*ignored*/0));
+    mainArea->addDockWidget(toolbarDockWidget, KDDockWidgets::Location_OnBottom, nullptr, QSize(/*ignored*/0, 96));
 
     return app.exec();
 }
